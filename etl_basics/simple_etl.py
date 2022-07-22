d32c9f7003f3c080ba012e702c6ac0a42055c25a
Olakusibe Aremu-Oluwole <olashod@gmail.com>
2022-07-22 13:08:00 +0100

    addition of comment to the conversion of data type

--- etl_basics/simple_etl.py
+++ etl_basics/simple_etl.py
@@ -26,10 +26,9 @@ zip_filename = 'datasource.zip'
 unzip_dir = 'dealership_data'
 
 if sys.platform == 'win32': # run windows cmd commands    
-    #os.system('wget ' + zip_download_url)
-    #os.system('mkdir ' + unzip_dir)
-    #os.system('tar -xf ' + zip_filename + ' --directory ' + unzip_dir)
-    print("nothing here")
+    os.system('wget ' + zip_download_url)
+    os.system('mkdir ' + unzip_dir)
+    os.system('tar -xf ' + zip_filename + ' --directory ' + unzip_dir)    
 elif sys.platform == 'linux': # run linux bash commands (NOT TESTED)
     os.system('wget ' + zip_download_url)
     os.system('unzip ' + zip_filename + ' -d ' + unzip_dir)
@@ -57,28 +56,23 @@ def extract_from_xml(file_to_process):
     dataframe = pd.DataFrame(columns=['car_model','year_of_manufacture','price','fuel'])
     tree = ET.parse(file_to_process)
     root = tree.getroot()
-    cars = []
-    years =[]
-    prices =[]
-    fuels=[]
+    
+    car_model = []
+    year_of_manufacture = []
+    price = []
+    fuel = []
+
     for person in root:
-        car_model = person.find("car_model").text
-        year_of_manufacture = int(person.find("year_of_manufacture").text)
-        price = float(person.find("price").text)
-        fuel = person.find("fuel").text
-
-        cars.append(car_model)        
-        years.append(year_of_manufacture)
-        prices.append(price)
-        fuels.append(fuel)
-        #dataframe = dataframe.append({"car_model":car_model, "year_of_manufacture":year_of_manufacture, "price":price, "fuel":fuel}, ignore_index = True)        
-
+        car_model.append(person.find("car_model").text)        
+        year_of_manufacture.append(int(person.find("year_of_manufacture").text))
+        price.append(float(person.find("price").text))
+        fuel.append(person.find("fuel").text)
 
     data = {
-        "car_model":cars, 
-        "year_of_manufacture":years,
-        "price":prices,
-        "fuel":fuels
+        "car_model":car_model, 
+        "year_of_manufacture":year_of_manufacture,
+        "price":price,
+        "fuel":fuel
         }
     df = pd.DataFrame(data)
     dataframe = pd.concat([dataframe, df], ignore_index = True)                
@@ -91,17 +85,17 @@ def extract():
     
     #process all csv files
     for csvfile in glob.glob("dealership_data/*.csv"):
-        #extracted_data = extracted_data.append(extract_from_csv(csvfile), ignore_index=True)
+        #extracted_data = extracted_data.append(extract_from_csv(csvfile), ignore_index=True) 'Deprecated
         extracted_data = pd.concat([extracted_data, extract_from_csv(csvfile)], ignore_index=True)
         
     #process all json files
     for jsonfile in glob.glob("dealership_data/*.json"):
-        #extracted_data = extracted_data.append(extract_from_json(jsonfile), ignore_index=True)
+        #extracted_data = extracted_data.append(extract_from_json(jsonfile), ignore_index=True) 'Deprecated
         extracted_data = pd.concat([extracted_data, extract_from_json(jsonfile)], ignore_index=True)
     
     #process all xml files
     for xmlfile in glob.glob("dealership_data/*.xml"):
-        #extracted_data = extracted_data.append(extract_from_xml(xmlfile), ignore_index=True)
+        #extracted_data = extracted_data.append(extract_from_xml(xmlfile), ignore_index=True)  'Deprecated
         extracted_data = pd.concat([extracted_data, extract_from_xml(xmlfile)], ignore_index=True)
         
     return extracted_data
@@ -153,6 +147,8 @@ log("starting Extract process")
 extracted_data = extract()
 print(extracted_data)
 
+# normalize/convert the data type in these columns to a specific type
+# especialy if there is a mix type to avoid AttributeError and TypeError
 convert_dict = {
     'year_of_manufacture': int,
     'price': float
